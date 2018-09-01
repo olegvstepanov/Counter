7b6e54f5c62ca0b77e9b2f765decad339003f5fe
Oleg Stepanov <oleg.v.stepanov@gmail.com>
2018-09-01 22:28:14 +0200

    add constructor method

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ import './index.css';
 
 class Counter extends React.Component {
 
-   
+    constructor (props) {
+        super(props);
+        this.state = 0;
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+        this.reset = this.reset.bind(this)
+    }
+
+    increment () {
+
+    }
+
+    decrement () {
+
+    }
+
+    reset () {
+
+    }
 
     render () {
         return <div> Hello! </div>;
