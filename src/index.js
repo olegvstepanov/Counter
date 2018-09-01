06c26bae5d0c1326b63de3a2e553ccf7f37a880d
Oleg Stepanov <oleg.v.stepanov@gmail.com>
2018-09-01 22:21:58 +0200

    add Counter class

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Counter extends React.Component {
 
+   
+
+    render () {
+        return <div> Hello! </div>;
+        
+
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
