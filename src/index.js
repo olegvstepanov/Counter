fb7935d2c5614d5f043316f1b829782ec75ffc15
Oleg Stepanov <oleg.v.stepanov@gmail.com>
2018-09-01 23:19:28 +0200

    add onClick handlers and corresponding methods

--- src/index.js
+++ src/index.js
@@ -6,29 +6,45 @@ class Counter extends React.Component {
 
     constructor (props) {
         super(props);
-        this.state = 0;
+        this.state = {
+            counter: 0
+        };
         this.increment = this.increment.bind(this)
         this.decrement = this.decrement.bind(this)
         this.reset = this.reset.bind(this)
     }
 
-    increment () {
 
+    increment () {
+        this.setState({
+            counter: this.state.counter + 1
+        })
     }
 
     decrement () {
-
+        this.setState({
+            counter: this.state.counter - 1
+        })
     }
 
     reset () {
-
+        this.setState({
+            counter: 0
+        })
     }
 
     render () {
-        return <div> Hello! </div>;
-        
+        return (
+            <div>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+                <button onClick={this.reset}>Reset</button>
+                <p>Value is: {this.state.counter}</p>
+            </div>
+        );     
 
     }
+
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
